4f1ec88e7c2e69e651913d15169ac8de016c19db
Jean-Marc Fontaine <jmf@spacelift.io>
2022-05-02 18:45:32 -0400

    Fail apply phase on purpose

--- submodule.tf
+++ submodule.tf
@@ -1,4 +1,11 @@
-module "s3_bucket" {
+module "s3_bucket1" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "spacelift-test-s3-bucket"
+  acl    = "private"
+}
+
+module "s3_bucket2" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "spacelift-test-s3-bucket"
